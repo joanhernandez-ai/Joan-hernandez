801c473432e6d64c2e19ba3a8b573529d842bafb
joanhernandez-ai <joanricardo888@gmail.com>
2024-11-27 08:17:27 -0600

    se sube todo
    subiendo todo

---
+++ ejemplos javascript/js/index.js
@@ -0,0 +1,12 @@
+// console.log("hola mundo como estas");
+// alert("ESTO ES UNA ADVERTENSIA PARA EL MUNDO");
+
+// console.log(5+300);
+// console.warn("ESTAMOS DANDO TIPS DE ALGEBRA WARNING") 
+// console.info("esto es muy important aca mucho");
+// console.error("Esto es un super error aca maquiavelico")
+
+
+<script src="js/index.js">
+</script>
+    
